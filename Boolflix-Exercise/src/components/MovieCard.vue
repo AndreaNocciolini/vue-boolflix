a29828953af9862baf3d60700111aaf3f30b1ff1
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-20 18:51:42 +0100

    add file not found img

--- Boolflix-Exercise/src/components/MovieCard.vue
+++ Boolflix-Exercise/src/components/MovieCard.vue
@@ -2,7 +2,7 @@
   <div class="col-3">
     <!-- <img src="" alt=""> -->
     <h1>Movie</h1>
-    <img :src="'https://image.tmdb.org/t/p/w342' + img" alt="">
+    <img :src="(img == null)? 'https://img.wallpapersafari.com/tablet/1536/2048/19/44/evOxST.jpg' :'https://image.tmdb.org/t/p/w300' + img" alt="">
     <h2>Title: {{title}}</h2>
     <h2>Original Title: {{originalTitle}}</h2>
     <h3>Language: <i :class="(language == 'en') ? 'flag flag-us' : 'flag flag-' + language"></i> {{language}}</h3>
@@ -23,6 +23,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  img {
+    width: 300px;
+    height: 450px;
+  }
 </style>
--- Boolflix-Exercise/src/components/TvSeriesCard.vue
+++ Boolflix-Exercise/src/components/TvSeriesCard.vue
@@ -2,7 +2,7 @@
   <div class="col-3">
     <!-- <img src="" alt=""> -->
     <h1>Tv Series</h1>
-    <img :src="'https://image.tmdb.org/t/p/w300' + img" alt="">
+    <img :src="(img == null)? 'https://img.wallpapersafari.com/tablet/1536/2048/19/44/evOxST.jpg' :'https://image.tmdb.org/t/p/w300' + img"  alt="">
     <h2>Title: {{name}}</h2>
     <h3>Original Title: {{originalName}}</h3>
     <h4>Language: <i :class="(language == 'en') ? 'flag flag-us' : 'flag flag-' + language"></i> {{language}}</h4>
@@ -23,6 +23,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  img {
+    width: 300px;
+    height: 450px;
+  }
 </style>
